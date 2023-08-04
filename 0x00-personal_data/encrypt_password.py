63bc839e4743d5ef23146759a176b9f00a756509
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-08-04 02:46:26 +0100

    implement checking if a password is valid with bcyrpt

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,10 @@ def hash_password(password: str) -> bytes:
     """
     hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    validates a password
+    """
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
