abdfb4e1904a5b7aea241edb6e640d787c290bf6
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-08-08 17:18:01 +0100

    implement requesting validation

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -37,7 +37,11 @@ class Auth:
         """
         returns None
         """
-        return None
+        if request is None:
+            return None
+        if request.headers.get('Authorization') is None:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
