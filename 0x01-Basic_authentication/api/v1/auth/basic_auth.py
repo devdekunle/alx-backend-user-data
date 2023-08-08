39124bb899d67a1177e4b18ff24a33eff526b4c7
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-08-08 18:10:42 +0100

    implement basic Basic64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,15 @@ class BasicAuth(Auth):
     """
     class that handles implementation of basic authentication
     """
-    pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        if authorization_header is None:
+            return None
+        if type(authorization_header) != str:
+            return None
+        if authorization_header.startswith('Basic ') is False:
+            return None
+        else:
+            token = authorization_header.split(' ')[1]
+            return token
