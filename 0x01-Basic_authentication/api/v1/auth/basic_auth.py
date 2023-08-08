67b8333a8aaa5edaabd5073a192bcfc5124465c7
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-08-08 17:35:29 +0100

    implement a basic authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module for implementation of Basic authentication
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    class that handles implementation of basic authentication
+    """
+    pass
