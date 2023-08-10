352b7ed4fafab3c0e3d04378a56c93ca82f065e7
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-08-10 23:17:27 +0100

    store authenticated user in request object and retrieve

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""
+Module for handling the user authentication
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """
+    class for handling authentication
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        checks if the path requuires a user authentication or not
+        """
+        if path is None:
+            return True
+
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        if path.endswith('/'):
+            if path in excluded_paths:
+                return False
+            else:
+                return True
+        else:
+            path = path + ('/')
+            if path in excluded_paths:
+                return False
+            else:
+                return True
+
+    def authorization_header(self, request=None) -> str:
+        """
+        returns None
+        """
+        if request is None:
+            return None
+        if request.headers.get('Authorization') is None:
+            return None
+        return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        returns None
+        """
+        return None
