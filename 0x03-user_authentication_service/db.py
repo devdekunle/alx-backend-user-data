62ece58c1170537d01d465198f962aca1db68d1f
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-08-15 17:56:12 +0100

    Find a user in the database based on arbitrary keyworded arguments and

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from user import Base, User
 from typing import TypeVar
 
@@ -41,4 +42,13 @@ class DB:
         # add the user to the database and save
         self._session.add(user)
         self._session.commit()
+        return user
+
+    def find_user_by(self, **kwargs):
+        """
+        find a user from the database using a keyworded argument
+        """
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if user is None:
+            raise NoResultFound
         return user
