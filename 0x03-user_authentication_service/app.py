9e3a06617aed4f1b3148d2df684bced2e5df0891
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-08-16 22:32:06 +0100

    implement login for user and set session cookie in response

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 A flask application
 """
-from flask import Flask, jsonify, make_response, request
+from flask import Flask, jsonify, make_response, request, abort
 from auth import Auth
 
 
@@ -32,6 +32,27 @@ def users():
                            "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """
+    logs a user in and creates a session
+    """
+    email = request.form.get("email")
+    if not email:
+        abort(401)
+    password = request.form.get("password")
+    if not password:
+        abort(401)
+    if AUTH.valid_login(email, password):
+        user_session_id = AUTH.create_session(email)
+        if user_session_id:
+            response = jsonify({"email": email, "message": "logged in"})
+            response.set_cookie("session_id", user_session_id)
+            return response
+        abort(401)
+    abort(401)
 
 
 if __name__ == '__main__':
