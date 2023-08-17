8ba56aaaccc0c8cd5bd8d3267f0790311bbd2581
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-08-17 20:09:51 +0100

    fix pycodestyle

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -95,6 +95,7 @@ def get_reset_password_token():
     except ValueError:
         abort(403)
 
+
 @app.route('/reset_password', methods=['PUT'], strict_slashes=False)
 def update_password():
     """
