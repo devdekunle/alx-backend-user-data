2801e239a00d1d8d16bdb68ee5b77263fbbe6077
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-08-17 20:08:52 +0100

    implement update password end point

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -95,6 +95,21 @@ def get_reset_password_token():
     except ValueError:
         abort(403)
 
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password():
+    """
+    update a user password
+    """
+    try:
+        email = request.form.get('email')
+        reset_token = request.form.get('reset_token')
+        new_password = request.form.get('new_password')
+        AUTH.update_password(reset_token, new_password)
+        return make_response({"email": email,
+                             "message": "Password updated"}), 200
+    except ValueError:
+        abort(403)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000', debug=True)
